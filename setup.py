bf0bd71e6161c26e17bf7a5d47914fcb7502534f
Jeremy Bowers <jeremyjbowers@gmail.com>
2015-07-09 23:05:04 -0400

    How about some modules.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name="pyopenfec",
+      version="0.2.0",
+      description="OpenFEC API client",
+      license="MIT",
+      install_requires=["requests"],
+      author="Jeremy Bowers",
+      author_email="jeremyjbowers@gmail.com",
+      url="http://github.com/jeremyjbowers/pyopenfec",
+      packages = find_packages(),
+      keywords= "fec campaign finance openfec",
+      classifiers=['Development Status :: 4 - Beta'],
+      zip_safe = True)
